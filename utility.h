1551ef368ff02b0eb64e55e41318639561c8e9fd
toughpig <wh19@mail.ustc.edu.cn>
2020-11-30 14:14:49 +0800

    utility

--- utility.h
+++ utility.h
@@ -1,1 +1,25 @@
+//STL提供
+//C++98 容器,迭代器,算法
+//C++11 正则表达式regex,rand/thread/async/future/time
+
+//STL效率优先,安全为次
+//错误检查的任务交给使用者
+//基本不使用异常,容易产生不明确行为
+
+#include<vector>
+#include<list>
+#include<forward_list>
+#include<string>
+#include<map>
+#include<array>
+#include<set>
+#include<deque>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+#include<random>
+#include<chrono>
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
 using namespace std;
